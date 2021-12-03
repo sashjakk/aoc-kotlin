ad8cd686212234d0187b34b718c41910464b4a35
sashjakk <sashjakk@gmail.com>
2021-12-03 15:06:12 +0200

    Day 02 - Move parts to local function to avoid package scoping problems

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -8,43 +8,43 @@ fun main() {
             direction to value.toInt()
         }
 
+    fun part1(input: List<Pair<String, Int>>): Int {
+        var horizontal = 0
+        var vertical = 0
+
+        for ((direction, value) in input) {
+            when (direction) {
+                "forward" -> horizontal += value
+                "up" -> vertical -= value
+                "down" -> vertical += value
+            }
+        }
+
+        return horizontal * vertical
+    }
+
     val result1 = part1(input)
     println("[Day 02 / 1] - $result1")
 
+    fun part2(input: List<Pair<String, Int>>): Int {
+        var aim = 0
+        var horizontal = 0
+        var vertical = 0
+
+        for ((direction, value) in input) {
+            when (direction) {
+                "forward" -> {
+                    horizontal += value
+                    vertical += aim * value
+                }
+                "up" -> aim -= value
+                "down" -> aim += value
+            }
+        }
+
+        return horizontal * vertical
+    }
+
     val result2 = part2(input)
     println("[Day 02 / 2] - $result2")
-}
-
-fun part1(input: List<Pair<String, Int>>): Int {
-    var horizontal = 0
-    var vertical = 0
-
-    for ((direction, value) in input) {
-        when (direction) {
-            "forward" -> horizontal += value
-            "up" -> vertical -= value
-            "down" -> vertical += value
-        }
-    }
-
-    return horizontal * vertical
-}
-
-fun part2(input: List<Pair<String, Int>>): Int {
-    var aim = 0
-    var horizontal = 0
-    var vertical = 0
-
-    for ((direction, value) in input) {
-        when (direction) {
-            "forward" -> {
-                horizontal += value
-                vertical += aim * value
-            }
-            "up" -> aim -= value
-            "down" -> aim += value
-        }
-    }
-
-    return horizontal * vertical
 }
